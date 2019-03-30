1f80d8af41b40ee1d42c31fbb32ed3426df731b9
Ryan Quanz <ryan.quanz@shopify.com>
2019-03-29 22:18:52 -0400

    do more in plugin

--- VocoderPlugin/Source/PluginProcessor.cpp
+++ VocoderPlugin/Source/PluginProcessor.cpp
@@ -21,13 +21,15 @@ using namespace std;
 #define N_BANDS 33
 // Q is set to have filter bandwith to be 1/3 octave
 #define Q_VALUE 4.318
-#define ENVELOPE_SAMPLE_SIZE 100
+#define ENVELOPE_SAMPLE_SIZE 10
 
 vector<IIRFilter> buildFilters();
 
 const float filter_frequencies[N_BANDS] = {12.5, 16, 20, 25, 31.5, 40, 50, 63, 80, 100, 125, 160, 200, 250, 315, 400, 500, 630, 800, 1000, 1250, 1600, 2000, 2500, 3150, 4000, 5000, 6300, 8000, 10000, 12500, 16000, 20000};
 
 vector<vector<float>> applyFilterBank(vector<float> input, vector<IIRFilter> filters, int num_samples);
+vector<vector<float>> applyEnvelope(vector<vector<float>> inputVector);
+vector<vector<float>> multiplyEnvelopes(vector<vector<float>> envelopes, vector<vector<float>> modulator);
 
 //==============================================================================
 VocoderPluginAudioProcessor::VocoderPluginAudioProcessor()
@@ -174,13 +176,31 @@ void VocoderPluginAudioProcessor::processBlock (AudioBuffer<float>& buffer, Midi
     auto modulatorPointer = buffer.getWritePointer(0);
     auto carrierPointer = buffer.getWritePointer(1);
     
-    // This could be a source of slowness, but copying the data out makes much nicer code.
     vector<float> modulatorData(modulatorPointer, modulatorPointer + sizeof(float) * buffer.getNumSamples());
     vector<float> carrierData(carrierPointer, carrierPointer + sizeof(float) * buffer.getNumSamples());
     
     auto filteredModulatorData = applyFilterBank(modulatorData, filters, buffer.getNumSamples());
     auto filteredCarrierData = applyFilterBank(modulatorData, filters, buffer.getNumSamples());
+    auto envelopedFilteredCarrierData = applyEnvelope(filteredCarrierData);
+    auto adjustedModulator = multiplyEnvelopes(envelopedFilteredCarrierData, filteredModulatorData);
     
+    copy(adjustedModulator[10].begin(), adjustedModulator[10].end(), modulatorPointer);
+}
+
+vector<vector<float>> multiplyEnvelopes(vector<vector<float>> envelopes, vector<vector<float>> modulator) {
+    vector<vector<float>> outputs;
+    
+    for(int i = 0; i < envelopes.size(); i++) {
+        auto envelope(envelopes[i]);
+        auto modulator(envelopes[i]);
+        vector<float> output;
+        
+        for(int j = 0; j < envelope.size(); j++) output.push_back(modulator[j] * envelope[j]);
+        
+        outputs.push_back(output);
+    }
+    
+    return outputs;
 }
 
 vector<IIRFilter> buildFilters() {
@@ -206,6 +226,29 @@ vector<vector<float>> applyFilterBank(vector<float> input, vector<IIRFilter> fil
     }
     
     return all_outputs;
+}
+
+vector<vector<float>> applyEnvelope(vector<vector<float>> inputVector) {
+    vector<vector<float>> output;
+    for (auto audioIter = inputVector.begin(); audioIter != inputVector.end(); ++audioIter) {
+        vector<float> audio(*audioIter);
+        vector<float> envelopedAudio;
+        
+        for(int audioIndex = 0; audioIndex < audio.size(); audioIndex += ENVELOPE_SAMPLE_SIZE) {
+            int samplesToCheck = min(ENVELOPE_SAMPLE_SIZE, (int)(audio.size() - audioIndex));
+            float maxValue = 0;
+            
+            for(int envelopeIndex = audioIndex; envelopeIndex < audioIndex + samplesToCheck; envelopeIndex++) {
+                float magnitude = fabs(audio[envelopeIndex]);
+                if(magnitude > maxValue) maxValue = magnitude;
+            }
+            
+            for(int i = 0; i < ENVELOPE_SAMPLE_SIZE; i++) envelopedAudio.push_back(maxValue);
+        }
+        output.push_back(envelopedAudio);
+    }
+
+    return output;
 }
 
 //==============================================================================
